4b9c0ec505946dcde806a8c6a6393d510deb6258
Deshelll <125933466+Deshelll@users.noreply.github.com>
2023-11-30 00:26:47 +0400

    Update AddSemicolons.js

--- obfuscator/src/AddSemicolons.js
+++ obfuscator/src/AddSemicolons.js
@@ -5,7 +5,7 @@ function addSemicolons(filePath) {
     try {
       let content = fs.readFileSync(filePath, 'utf-8');
 
-      content = content.replace(/}\s*(?=\w)(?!(\s*else))/g, '}; ');
+      content = content.replace(/}\s*(?=\w)(?!(\s*else|\s*catch))/g, '}; ');
   
       fs.writeFileSync(filePath, content, 'utf-8');
   
